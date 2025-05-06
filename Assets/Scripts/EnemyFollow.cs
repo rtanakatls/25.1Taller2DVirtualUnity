579500d99092ff73bd0139146f260da89ab2d68e
Ricardo Tanaka Terukina <rtanaka92@gmail.com>
2025-05-06 18:16:09 -0500

    Added patrol

--- Assets/Scripts/EnemyFollow.cs
+++ Assets/Scripts/EnemyFollow.cs
@@ -5,9 +5,15 @@ public class EnemyFollow : MonoBehaviour
     private Transform target;
     private Rigidbody2D rb2d;
     [SerializeField] private float speed;
+    [SerializeField] private float range;
+
+    private float timer;
+    private float patrolDirection;
+    [SerializeField] private float patrolTime;
 
     private void Awake()
     {
+        patrolDirection = transform.localScale.x;
         rb2d = GetComponent<Rigidbody2D>();
     }
 
@@ -32,17 +38,25 @@ public class EnemyFollow : MonoBehaviour
         {
             Vector2 scale = transform.localScale;
             Vector2 direction = target.transform.position - transform.position;
-            if (scale.x > 0 && transform.position.x < target.transform.position.x)
+            if (Vector2.Distance(transform.position, target.position) < range && scale.x > 0 && transform.position.x < target.transform.position.x)
             {
                 rb2d.linearVelocity = direction.normalized * speed;
             }
-            else if(scale.x < 0 && transform.position.x > target.transform.position.x)
+            else if (Vector2.Distance(transform.position, target.position) < range && scale.x < 0 && transform.position.x > target.transform.position.x)
             {
                 rb2d.linearVelocity = direction.normalized * speed;
             }
             else
             {
-                rb2d.linearVelocity = Vector2.zero;
+                rb2d.linearVelocity = Vector2.right * patrolDirection * speed;
+                timer += Time.deltaTime;
+                if (timer >= patrolTime)
+                {
+                    timer = 0;
+                    patrolDirection *= -1;
+                    scale.x = patrolDirection;
+                    transform.localScale=scale; 
+                }
             }
             
         }
@@ -52,4 +66,9 @@ public class EnemyFollow : MonoBehaviour
         }
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
 }
